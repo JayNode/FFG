997493ba70d59f411a3203290556f810981b323a
Jared Immerman <jimmerman819@gmail.com>
2023-10-06 22:58:29 -0700

    commiting

--- fabled/src/components/slider/Slider-data.js
+++ fabled/src/components/slider/Slider-data.js
@@ -1,22 +1,39 @@
-import Img1 from "../../images/header/Woods-Header.jpg";
-import Img2 from "../../images/header/Fall-Header.jpg";
-import Img3 from "../../images/header/TinyTree-Header.jpg";
-
+import Img1 from "../../images/header/FallForest-Header.jpg";
+import Img2 from "../../images/header/FlowerView-Header.jpg";
+import Img3 from "../../images/header/IslandHome-Header.jpg";
+import Img4 from "../../images/header/LakeMountain-Header.jpg";
+import Img5 from "../../images/header/Matterhorn-Header.jpg";
+import Img6 from "../../images/header/TinyTree-Header.jpg";
 
 export const sliderData = [
-    {
-        image: Img1,
-        heading: "Forest",
-        desc: "",
-    },
-    {
-        image: Img2,
-        heading: "Mountains",
-        desc: "",
-    },
-    {
-        image: Img3,
-        heading: "Foliage",
-        desc: "",
-    },
-];   
+  {
+    image: Img1,
+    heading: "Forest",
+    desc: "",
+  },
+  {
+    image: Img2,
+    heading: "Flower",
+    desc: "",
+  },
+  {
+    image: Img3,
+    heading: "Island",
+    desc: "",
+  },
+  {
+    image: Img4,
+    heading: "Lake",
+    desc: "",
+  },
+  {
+    image: Img5,
+    heading: "Mountains",
+    desc: "",
+  },
+  {
+    image: Img6,
+    heading: "Tree",
+    desc: "",
+  },
+];
--- fabled/src/components/slider/Slider.js
+++ fabled/src/components/slider/Slider.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
-import { AiOutlineArrowLeft,AiOutlineArrowRight } from "react-icons/ai"
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { sliderData } from "./Slider-data";
 import "./Slider.css";
 
 const Slider = () => {
-
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideLength = sliderData.length;
   // slideLength = 1 2 3
   // currentSlide = 0 1 2
 
-  const autoScroll = false;
+  const autoScroll = true;
   let slideInterval;
-  let intervalTime = 20000;
+  let intervalTime = 60000;
 
   //Next slide or currentSlide = 0
   const nextSlide = () => {
-    setCurrentSlide(currentSlide === (slideLength - 1) ? 
-      0 : 
-      currentSlide + 1
-    );
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
   };
 
   //Prev slide or currentSlide = slideLength-1
   const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ?
-      slideLength - 1 :  
-      currentSlide - 1    
-    );
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
   };
 
   function auto() {
@@ -37,8 +30,8 @@ const Slider = () => {
 
   //Initial: currentSlide = 0
   useEffect(() => {
-    setCurrentSlide(0)
-  },[]);
+    setCurrentSlide(0);
+  }, []);
 
   //Auto next slide after (intervalTime) seconds
   //slideInterval = 0 after next/prev slide
@@ -50,34 +43,32 @@ const Slider = () => {
   });
 
   return (
-      <div className="slider">
-        <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide}/>
-        <AiOutlineArrowRight className="arrow next" onClick={nextSlide}/>
+    <div className="slider">
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
 
-        {sliderData.map((slide, index) => {
-          return (
-            <div className={index === currentSlide ?
-            "slide current" : "slide"} 
+      {sliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === currentSlide ? "slide current" : "slide"}
             key={index}
-            >
-              
-              {index === currentSlide && (
-                <div>
-                  <img src={slide.image} alt="slide" /> 
-                  
-                  <div className="slide-content">
-                    <h2>{slide.heading}</h2>
-                    <p>{slide.desc}</p>
-                    <hr />
-                    <button className="slide-button">Get Started</button>
-                  </div>
-                </div>
-              )}
-            </div>
-          )
-        })}
+          >
+            {index === currentSlide && (
+              <div>
+                <img src={slide.image} alt="slide" />
 
-      </div>
+                <div className="slide-content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.desc}</p>
+                  <hr />
+                  <button className="slide-button">Get Started</button>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
