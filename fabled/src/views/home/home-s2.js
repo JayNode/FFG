cf80621d535d5cbdff4cbaceb12cd5ffae248eed
Jared Immerman <jimmerman819@gmail.com>
2023-10-12 17:07:22 -0700

    sect 2 flex change

--- fabled/src/views/home/home-s2.js
+++ fabled/src/views/home/home-s2.js
@@ -3,51 +3,58 @@ import Island from "../.././images/header/IslandHome-Header.jpg";
 import Fall from "../.././images/header/FallForest-Header.jpg";
 import Matterhorn from "../.././images/header/Matterhorn-Header.jpg";
 
-import Stack from "@mui/material/Stack";
-import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
 
 const SectTwo = () => {
   return (
-    <Stack
+    <Grid 
       className="content section2" 
-      direction="row" 
-      justifyContent="space-around" 
-      alignItems="flex-start" 
-      spacing={10}
+      container 
+      justifyContent="space-evenly"
+      alignItems="flex-start"
+      spacing={3} 
+      sx={{ FlexDirection: { xs: "column", md: "row"} }}
     >
-      <Container>
+      <Grid item xs={7} md={4}>
         <div className="news-container sect2-news1">
           <img className="news-img" src={Island} alt="island" />
-          <h1>Island resouces plummet after rise in tourism</h1>
+          <Link className="news-link-title" href="" underline="hover">
+            <h1>Island resouces plummet after rise in tourism</h1>
+          </Link>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </div>  
-      </Container>
+      </Grid>
 
-      <Container>
+      <Grid item xs={7} md={4}>
         <div className="news-container sect2-news2">
           <img className="news-img" src={Fall} alt="fall" />
-          <h1>Leaves change color for first time in California</h1>
+          <Link className="news-link-title" href="" underline="hover">
+            <h1> Leaves change color for first time in California </h1>
+          </Link>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </div>
-      </Container>
+      </Grid>
 
-      <Container>
+      <Grid item xs={7} md={4}>
         <div className="news-container sect2-news3">
           <img className="news-img" src={Matterhorn} alt="matterhorn" />
-          <h1>Climber trapped, rescuers racing time</h1>
+          <Link className="news-link-title" href="" underline="hover">
+            <h1>Climber trapped, rescuers racing time</h1>
+          </Link>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </div>
-      </Container>
-    </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
