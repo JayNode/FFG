42b139ddbead90e0ad539c746e1922ea3c44950f
Jared Immerman <jimmerman819@gmail.com>
2023-10-14 20:26:34 -0700

    home page cleanup

--- fabled/src/views/home/home-s1.js
+++ fabled/src/views/home/home-s1.js
@@ -11,74 +11,66 @@ import Container from "@mui/material/Container";
 const SectOne = () => {
   return (
     <Stack className="content section1" direction="row">
-      <Container>
-        <div id="sect1-intro">
-          <h1>FABLED FEILDGUIDE</h1>
-          <h2>Who we are</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
+      <Container id="sect1-intro">
+        <h2>Who we are</h2>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <Button id="intro-bttn" variant="outlined">
+          Mission
+        </Button>
       </Container>
 
-      <Container>
-        <div id="sect1-categories">
-          <Stack direction="row" alignItems="center" spacing={10} >
-            <img className="sect1-img" src={bigfoot} alt="bigfoot" />
-            <div>
-              <Stack direction="row" justifyContent="space-between" spacing={1}>
-                <h1>Beasts</h1>
-                <div className="sect1-button">
-                  <Button variant="outlined">Catalog</Button>
-                </div>
-              </Stack>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </Stack>
+      <Container id="sect1-categories">
+        <Stack direction="row" alignItems="center" spacing={10}>
+          <img className="sect1-img" src={bigfoot} alt="bigfoot" />
+          <div>
+            <Stack direction="row" justifyContent="space-between" spacing={1}>
+              <h1>Beasts</h1>
+              <Button variant="outlined">Catalog</Button>
+            </Stack>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </Stack>
 
-          <hr className="sect1-divider"/>
+        <hr className="sect1-divider" />
 
-          <Stack direction="row" alignItems="center" spacing={10} >
-            <img className="sect1-img" src={cave} alt="cave" />
-            <div>
-              <Stack direction="row" justifyContent="space-between" spacing={1} >
-                <h1>Environment</h1>
-                <div className="sect1-button">
-                  <Button variant="outlined">Habitats</Button>
-                </div>
-              </Stack>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </Stack>
+        <Stack direction="row" alignItems="center" spacing={10}>
+          <img className="sect1-img" src={cave} alt="cave" />
+          <div>
+            <Stack direction="row" justifyContent="space-between" spacing={1}>
+              <h1>Environment</h1>
+              <Button variant="outlined">Habitats</Button>
+            </Stack>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </Stack>
 
-          <hr className="sect1-divider"/>
+        <hr className="sect1-divider" />
 
-          <Stack direction="row" alignItems="center" spacing={10} >
-            <img className="sect1-img" src={scroll} alt="scroll" />
-            <div>
-              <Stack direction="row" justifyContent="space-between" spacing={1} >
-                <h1>Fieldguide</h1>
-                <div className="sect1-button">
-                  <Button variant="outlined">Adventure</Button>
-                </div>
-              </Stack>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </Stack>
-        </div>
+        <Stack direction="row" alignItems="center" spacing={10}>
+          <img className="sect1-img" src={scroll} alt="scroll" />
+          <div>
+            <Stack direction="row" justifyContent="space-between" spacing={1}>
+              <h1>Fieldguide</h1>
+              <Button variant="outlined">Adventure</Button>
+            </Stack>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </Stack>
       </Container>
     </Stack>
   );
--- fabled/src/views/home/home-s2.js
+++ fabled/src/views/home/home-s2.js
@@ -4,65 +4,74 @@ import Fall from "../.././images/header/FallForest-Header.jpg";
 import Matterhorn from "../.././images/header/Matterhorn-Header.jpg";
 
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 
 const SectTwo = () => {
   return (
-    <Grid
-      container
-      className="content section2"
-      justifyContent="space-around"
-      alignItems="flex-start"
-      spacing={5}
-      sx={{ Direction: { xs: "column", md: "row" } }}
-    >
-      <Grid container item xs={12} md={2}>
-        <Grid item xs={6} md>
-          <img className="news-img" src={Island} alt="island" />
-        </Grid>
-        <br />
-        <Grid item xs={6} md>
-          <Link className="news-link-title" href="" underline="hover">
-            <h1>Island resouces plummet after rise in tourism</h1>
-          </Link>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </Grid>
-      </Grid>
+    <div className="content section2">
+      <Container>
+        <div className="sect2-header">
+          <h1>News Stories</h1>
+        </div>
+        <Grid
+          container
+          justifyContent="space-around"
+          alignItems="flex-start"
+          spacing={5}
+          sx={{ FlexDirection: { xs: "column", md: "row" } }}
+        >
+          <Grid container item xs={12} md={4}>
+            <Grid item xs={6} md={12}>
+              <img className="news-img" src={Island} alt="island" />
+              <hr className="img-line" />
+            </Grid>
+            <br />
+            <Grid item xs={6} md={12}>
+              <Link className="news-link-title" href="" underline="hover">
+                <h1>Island resouces plummet after rise in tourism</h1>
+              </Link>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Grid>
+          </Grid>
 
-      <Grid container item xs={12} md={2}>
-        <Grid item xs={6} md>
-          <img className="news-img" src={Fall} alt="fall" />
-        </Grid>
-        <Grid item xs={6} md>
-          <Link className="news-link-title" href="" underline="hover">
-            <h1> Leaves change color for first time in California </h1>
-          </Link>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </Grid>
-      </Grid>
+          <Grid container item xs={12} md={4}>
+            <Grid item xs={6} md={12}>
+              <img className="news-img" src={Fall} alt="fall" />
+              <hr className="img-line" />
+            </Grid>
+            <Grid item xs={6} md={12}>
+              <Link className="news-link-title" href="" underline="hover">
+                <h1> Leaves change color for first time in California </h1>
+              </Link>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Grid>
+          </Grid>
 
-      <Grid container item xs={12} md={2}>
-        <Grid item xs={6} md>
-          <img className="news-img" src={Matterhorn} alt="matterhorn" />
+          <Grid container item xs={12} md={4}>
+            <Grid item xs={6} md={12}>
+              <img className="news-img" src={Matterhorn} alt="matterhorn" />
+              <hr className="img-line" />
+            </Grid>
+            <Grid item xs={6} md={12}>
+              <Link className="news-link-title" href="" underline="hover">
+                <h1>Climber trapped, rescuers racing time</h1>
+              </Link>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6} md>
-          <Link className="news-link-title" href="" underline="hover">
-            <h1>Climber trapped, rescuers racing time</h1>
-          </Link>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </Grid>
-      </Grid>
-    </Grid>
+      </Container>
+    </div>
   );
 };
 
