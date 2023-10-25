93c5a6a4c696fc596a2ce66a9e0bfd3064461189
Jared Immerman <jimmerman819@gmail.com>
2023-10-24 18:45:05 -0700

    random commit

--- fabled/src/components/itemCard/habitatCard.js
+++ fabled/src/components/itemCard/habitatCard.js
@@ -2,13 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./habitatCard.css";
 
-import Bigfoot from "../.././images/sticker/bigfoot-sticker.png";
-import Yeti from "../.././images/sticker/yeti-sticker.png";
-import Kraken from "../.././images/sticker/kraken-sticker.png";
-import Dragon from "../.././images/sticker/dragon-sticker.png";
-import Vampire from "../.././images/sticker/vampire-sticker.png";
-import Skeleton from "../.././images/sticker/skull-sticker.png";
-
 import Cave from "../.././images/sticker/cave-sticker.png";
 import Lake from "../.././images/sticker/lake-sticker.png";
 import Snowcave from "../.././images/sticker/snowcave-sticker.png";
